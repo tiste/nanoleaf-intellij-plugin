d23d39d6290d6e1643a6fdbd4523702e97fe100d
GitHub Action <action@github.com>
2022-05-06 13:57:22 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tiste/nanoleafintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tiste.nanoleafintellijplugin.services
+
+import com.github.tiste.nanoleafintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
